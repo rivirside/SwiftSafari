1e84fe1a06ffa44f657b767e0dadf3aa1d93cecd
oceanexplains <121273516+oceanexplains@users.noreply.github.com>
2022-12-31 16:59:35 -0500

    Initial Commit

---
+++ SwiftSafari/SwiftSafariApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSafariApp.swift
+//  SwiftSafari
+//
+//  Created by Tomer Zilbershtein on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
